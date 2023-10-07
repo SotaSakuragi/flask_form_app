86e6b06b9fcb7e9d21eda0087ec7a0fde22f2339
SotaSakuragi <ebi.ebi.kani.kani.ikura@gmail.com>
2023-10-07 14:21:38 +0900

    リファクタリング

--- app.py
+++ app.py
@@ -4,15 +4,17 @@ app = Flask(__name__)
 
 
 # GETメソッド、POSTメソッドの追記
-@app.route("/", methods=["GET", "POST"])
-def index():
-    # もしGETリクエストだった場合はindex.htmlを表示
-    if request.method == "GET":
-        return render_template("index.html")
-    else:
-        # もしPOSTリクエストだった場合はindex.htmlでmessage変数を使う
-        message = request.form['message']
-        return render_template("index.html", message=message)
+# @app.route("/", methods=["GET"])
+@app.route("/")
+def index_get():
+    return render_template("index.html")
+
+
+# @app.route("/", methods=["POST"])
+@app.route("/")
+def index_post():
+    message = request.form["message"]
+    return render_template("index.html", message=message)
 
 
 if __name__ == "__main__":
